b490ae38fb8551ff7e4e7d56dc504d79a20c9b08
Maxime Andro <maxime@ixamem.com>
2025-05-20 16:28:06 +0200

    exo 2 updade

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,20 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  # 1. INDEX
+  get   "/tasks",          to: "tasks#index", as: "tasks"
+
+  # 2. NEW et CREATE
+  get   "/tasks/new",      to: "tasks#new",    as: "new_task"
+  post  "/tasks",          to: "tasks#create"
+
+  # 3. SHOW
+  get   "/tasks/:id",      to: "tasks#show",   as: "task"
+
+  # 4. EDIT et UPDATE
+  get   "/tasks/:id/edit", to: "tasks#edit",   as: "edit_task"
+  patch "/tasks/:id",      to: "tasks#update"
+
+  # 5. DESTROY
+  delete "/tasks/:id",     to: "tasks#destroy"
 end
